4045c016d33b153730c0ab1f59d96c0a57b000fe
Toby Ueno <uenot@go.oes.edu>
2020-08-25 19:07:31 -0700

    added hangman

---
+++ game.py
@@ -0,0 +1,111 @@
+import requests
+import random
+
+
+class Hangman:
+    def __init__(self):
+        words = requests.get('https://users.cs.duke.edu/~ola/ap/linuxwords').text.split()
+        self.words = [word.lower().strip() for word in words if not word[0].isupper() and len(word) >= 3]
+        self.reset()
+
+    def reset(self):
+        self.word = [[char, False] for char in list(random.choice(self.words))]
+        self.wrong_guesses_left = 6
+        self.guesses = []
+        self.started = False
+
+    def guess(self, letter):
+        matching_chars = 0
+        self.guesses.append(letter)
+        for char in self.word:
+            if not char[1] and char[0] == letter:
+                char[1] = True
+                matching_chars += 1
+        if matching_chars == 0:
+            self.wrong_guesses_left -= 1
+
+    def get_knowns(self):
+        knowns = ''
+        for char in self.word:
+            if char[1]:
+                knowns += char[0] + ' '
+            else:
+                knowns += '_ '
+        knowns.strip()
+        return knowns
+
+    def get_word(self):
+        return ''.join([char[0] for char in self.word])
+
+    def check_victory(self):
+        for char in self.word:
+            if not char[1]:
+                return False
+        return True
+
+    def check_loss(self):
+        if self.wrong_guesses_left > 0:
+            return False
+        return True
+
+    def get_possible_words(self):
+        possible_words = []
+        for word in self.words:
+            if len(word) == len(self.word):
+                matching_known_chars = True
+                for i, char in enumerate(self.word):
+                    if (char[1] and char[0] != word[i]) or (not char[1] and word[i] in self.guesses):
+                        matching_known_chars = False
+                        break
+                if matching_known_chars:
+                    possible_words.append(word)
+        return possible_words
+
+    def cheat(self):
+        possible_words = self.get_possible_words()
+        if len(possible_words) >= 1:
+            possible_words.append(self.get_word())
+            new_word = random.choice(possible_words)
+            for i, char in enumerate(self.word):
+                char[0] = new_word[i]
+
+    def check_input(self, input_str):
+        if len(input_str) == 1 and input_str.isalpha():
+            return True
+        return False
+
+    def start(self):
+        self.started = True
+        message = self.get_knowns()
+        message += '\n\nTo guess, counter this trade and enter a letter in the trade note area.'
+        return message
+
+    def action(self, input_str):
+        if not self.started:
+            return self.start()
+        if self.check_input(input_str):
+            input_str = input_str.lower()
+            self.guess(input_str)
+            message = self.get_knowns()
+            message += f'\tGuesses left: {self.wrong_guesses_left} \t Previous guesses: {", ".join(self.guesses)}'
+            if self.check_victory():
+                message += '\n\nYou won!'
+                self.reset()
+                message += '\n\nTo play again, submit another trade.'
+            elif self.check_loss():
+                message += '\n\nYou ran out of guesses.'
+                message += f'The word was: {self.get_word()}'
+                self.reset()
+                message += '\n\nTo play again, submit another trade.'
+            else:
+                message += '\n\nTo guess, counter this trade and enter a letter in the trade note area.'
+        else:
+            message = f'The input {input_str} was invalid. Type exactly one letter into the trade note box.'
+        return message
+
+
+if __name__ == '__main__':
+    game = Hangman()
+    while not game.check_victory() and not game.check_loss():
+        print(game.action(input()))
+
--- traderbot.py
+++ traderbot.py
@@ -1,3 +1,4 @@
+import json
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
@@ -10,6 +11,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from getpass import getpass
 import requests
+from game import *
 
 
 def id_to_name(player_id):
@@ -28,9 +30,10 @@ def get_players_from_page(driver):
 
 
 class Trade:
-    def __init__(self, url, driver):
+    def __init__(self, url, team_id, driver):
         self.url = url
         self.league_id = self.url.split('/')[-3]
+        self.team_id = str(team_id)
         self.driver = driver
 
     def is_active(self):
@@ -44,12 +47,12 @@ class Trade:
         except TimeoutException:
             return False
 
-    def get_players(self):
+    def get_players_by_team(self):
         my_players = []
         other_players = []
         if self.is_active():
             all_players = get_players_from_page(self.driver)
-            self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/3')
+            self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{self.team_id}')
             my_team = get_players_from_page(self.driver)
             for player in all_players:
                 if player in my_team:
@@ -58,10 +61,43 @@ class Trade:
                     other_players.append(player)
         return my_players, other_players
 
+    def get_players(self):
+        my_players, other_players = self.get_players_by_team()
+        return my_players + other_players
+
+    def get_teams(self):
+        players = []
+        if self.is_active():
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            anchors = soup.find_all('a', href=True)
+            for anchor in anchors:
+                anchor_test_list = anchor['href'].split('/')
+                if anchor_test_list[:-1] == ['', 'f1', self.league_id] and anchor_test_list[-1].isnumeric():
+                    players.append(anchor['href'].split('/')[-1])
+        return list(set(players))
+
+    def get_other_team(self):
+        teams = self.get_teams()
+        for team in teams:
+            if team != self.team_id:
+                return team
+        return None
+
+    def get_message(self):
+        if self.is_active():
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            message = soup.find('div', class_='tradenote')
+            if message is None:
+                message = ''
+            else:
+                message = message.find('p').text
+            return message
+        return ''
+
     def was_received(self):
         if self.is_active():
             try:
-                reject_btn = self.driver.find_element_by_link_text('Reject Trade')
+                self.driver.find_element_by_link_text('Reject Trade')
                 return True
             except NoSuchElementException:
                 return False
@@ -76,7 +112,7 @@ class Trade:
 
 
 class TraderBot:
-    def __init__(self, league_id, headless=False):
+    def __init__(self, league_id, team_id, headless=False):
         # initialize options
         chrome_options = Options()
         if headless:
@@ -84,9 +120,10 @@ class TraderBot:
             chrome_options.add_argument('--window-size=1920x1080')
             chrome_options.add_argument('--remote-debugging-port=9222')
         # initialize driver
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.league_id = league_id
-        self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/3')
+        self.team_id = team_id
+        self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{self.team_id}')
 
         if headless:
             # have to login through command prompt if headless
@@ -99,6 +136,15 @@ class TraderBot:
                 EC.presence_of_element_located((By.ID, 'login-passwd'))
             )
             password_field.send_keys(getpass() + Keys.ENTER)
+
+            # will sometimes ask if you want to link FB Messenger on startup
+            try:
+                skip_messenger = WebDriverWait(self.driver, 3).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="fb-messenger-linking"]/form[2]/div/a'))
+                )
+                skip_messenger.click()
+            except TimeoutException:
+                pass
         else:
             # if not headless, should login through window
             input('confirm login')
@@ -106,9 +152,11 @@ class TraderBot:
     def get_trades(self):
         trades = []
         i = 1
+        home_url = f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{self.team_id}'
+        self.driver.get(home_url)
         while True:
-            if self.driver.current_url != f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/3':
-                self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/3')
+            if self.driver.current_url != home_url:
+                self.driver.get(home_url)
             try:
                 team_notes = WebDriverWait(self.driver, 3).until(
                     EC.presence_of_element_located((By.XPATH, f'//*[@id="teamnotes"]/div/div[{i}]'))
@@ -125,36 +173,24 @@ class TraderBot:
                 WebDriverWait(self.driver, 4).until(
                     EC.presence_of_element_located((By.LINK_TEXT, 'Evaluate Trade'))
                 )
-                trades.append(Trade(self.driver.current_url, self.driver))
+                trades.append(Trade(self.driver.current_url, self.team_id, self.driver))
                 i += 1
             except TimeoutException:
                 i += 1
                 continue
         return trades
 
-    def cancel_trades(self, target_players=None, log=False):
-        """
-        Cancels some or all active trades.
+    def team_id_to_name(self, team_id):
+        self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{team_id}')
 
-        :param target_players: List of player IDs. Any trade with only these players will be canceled.
-        If not specified, cancels all trades.
-        :param method: Specify whether to cancel sent trades, reject received trades, or both.
-        :param message: A message to send upon rejecting a trade. Applies only to trades received; not to those sent.
-        :param log: Specify whether to print cancelled trades to the console or not.
-        """
+        team_name = self.driver.find_element_by_xpath('//*[@id="team-card-info"]/div[2]/ul/li/a')
+        team_name = ' '.join(team_name.text.strip().split()[:-2])
 
-        for trade in self.get_trades():
-            my_players, other_players = trade.get_players()
-            if target_players is None or sorted(target_players) == sorted(my_players + other_players):
-                if log:
-                    log_str = f'{", ".join([id_to_name(player) for player in my_players])} for '
-                    log_str += f'{", ".join([id_to_name(player) for player in other_players])}'
-                    print(log_str)
-                trade.cancel()
+        return team_name
 
     def permacancel(self, interval, method=None):
         """
-        Runs cancel_trades within a while loop.
+        Cancels all trades repeatedly.
 
         :param interval: The number of seconds between cancels.
         :param method: Specify whether to cancel sent trades, reject received trades, or both.
@@ -162,29 +198,23 @@ class TraderBot:
         i = 0
         while True:
             i += 1
-            self.cancel_trades(log=True)
+            for trade in self.get_trades():
+                received = trade.was_received()
+                if method is None or (method == 'Reject' and received) or (method == 'Cancel' and not received):
+                    trade.cancel()
             time.sleep(interval)
 
-    def submit_trade(self, other_team, players, message=''):
-        """
-        Submits a trade.
-        
-        :param other_team: ID of the team to trade with.
-        :param players: List of players to trade (on both teams; order doesn't matter)
-        :param message: Custom message to send along with the trade.
-        """
-        # navigate to page of target team, start trade creation
-        self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{other_team}')
-        create_trade_button = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.LINK_TEXT, 'Create Trade'))
-        )
-        create_trade_button.click()
+    def fill_and_submit_trade(self, players, message):
+        # assumes that the current page is the "select players" page
 
         # explicit wait for continue btn: consistent element across all trade pages, also used later
         # located here in order to test for page load
-        continue_button = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.LINK_TEXT, 'Continue'))
-        )
+        try:
+            continue_button = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.LINK_TEXT, 'Continue'))
+            )
+        except TimeoutException:
+            return False
 
         # iterate through all players in list; select all for trade
         # team doesn't matter
@@ -205,6 +235,141 @@ class TraderBot:
             EC.presence_of_element_located((By.LINK_TEXT, 'Send Trade Proposal'))
         )
         send_trade_button.click()
+
+        # find the sent trade and return it
+        possible_trades = []
+        for trade in self.get_trades():
+            if not trade.was_received():
+                if sorted(trade.get_players()) == sorted(players):
+                    possible_trades.append(trade)
+
+        possible_trades.sort(key=lambda x: int(x.url.split('=')[-1]), reverse=True)
+        try:
+            return possible_trades[0]
+        except IndexError:
+            return None
+
+    def create_trade(self, other_team, players, message=''):
+        """
+        Submits a trade.
+        
+        :param other_team: ID of the team to trade with.
+        :param players: List of players to trade (on both teams; order doesn't matter)
+        :param message: Custom message to send along with the trade.
+        """
+        # navigate to page of target team, start trade creation
+        self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{other_team}')
+        create_trade_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, 'Create Trade'))
+        )
+        create_trade_button.click()
+
+        return self.fill_and_submit_trade(players=players, message=message)
+
+    def counter_trade(self, trade, players, message=''):
+        if trade.is_active() and trade.was_received():
+            counter_trade_button = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.LINK_TEXT, 'Make Counter Offer'))
+            )
+            counter_trade_button.click()
+
+            return self.fill_and_submit_trade(players=players, message=message)
+        return None
+
+    def run_hangman(self, log=True):
+        with open('junktrades/trades_to_send.json') as f:
+            trades_to_send = json.load(f)
+
+        games = {team: Hangman() for team in trades_to_send.keys()}
+
+        while True:
+            for trade in self.get_trades():
+                if trade.was_received():
+                    other_team = trade.get_other_team()
+                    game = games[other_team]
+                    response = trade.get_message()
+                    prompt = game.action(response)
+                    self.counter_trade(trade, trades_to_send[other_team], message=prompt)
+                    if log:
+                        print(f'{self.team_id_to_name(other_team)}: {game.get_knowns()} ({game.get_word()})')
+            time.sleep(10)
+
+    def generate_junk_trades(self):
+        # change this to run twice and cross-reference results
+        # to avoid injuries
+        trades_to_send = {}
+        trades_to_receive = {}
+        i = 1
+        while True:
+            if i == self.team_id:
+                i += 1
+                continue
+
+            self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{i}')
+
+            # breaks after hitting the last team
+            players = get_players_from_page(self.driver)
+            if len(players) == 0:
+                break
+
+            # switch to projected stats for this season
+            projected_stats_button = self.driver.find_element_by_id('P')
+            projected_stats_button.click()
+            this_season_button = self.driver.find_element_by_xpath('//*[@id="subnav_P"]/li[4]/a')
+            this_season_button.click()
+
+            # TODO: click the projected stats/ whole season (rn 2019 results)
+            player_list = []
+            # takes advantage of the fact that get_players_from_page returns in order of appearance on page
+            for j, player in enumerate(players, 1):
+                # note: doesn't get kickers/defenses
+                # those are in different tables (statTable1, etc.)
+                proj_pt_xpath = f'//*[@id="statTable0"]/tbody/tr[{j}]/td[6]/div/span'
+                try:
+                    player_proj_pts = self.driver.find_element_by_xpath(proj_pt_xpath)
+                except NoSuchElementException:
+                    # will occur for kickers/defenses
+                    break
+                player_list.append((player, float(player_proj_pts.text)))
+
+            # get player w/ highest proj. pts
+            trades_to_send[i] = [sorted(player_list, key=lambda x: -x[1])[0][0]]
+            trades_to_receive[i] = [sorted(player_list, key=lambda x: x[1])[0][0]]
+
+            i += 1
+
+        self.driver.get(f'https://football.fantasysports.yahoo.com/f1/{self.league_id}/{self.team_id}')
+        my_players = get_players_from_page(self.driver)
+
+        # gets the third to last player on the page
+        # usually the last bench slot
+        # will be different if team doesn't have a kicker or defense
+        player_to_trade_away = my_players[-3]
+        for trade in trades_to_send.values():
+            trade.append(player_to_trade_away)
+
+        # gets the first player (usually qb)
+        player_to_trade_away = my_players[0]
+        for trade in trades_to_receive.values():
+            trade.append(player_to_trade_away)
+
+        with open('junktrades/trades_to_send.json', 'w') as f:
+            json.dump(trades_to_send, f, indent=4)
+
+        with open('junktrades/trades_to_receive.json', 'w') as f:
+            json.dump(trades_to_receive, f, indent=4)
+
+    def view_junk_trades(self):
+        print('Trades to send:')
+        with open('junktrades/trades_to_send.json') as f:
+            trades = json.load(f)
+        for team, players, in trades.items():
+            print(f'Team {team}: {[id_to_name(player) for player in players]}')
+        print('\nTrades to receive:')
+        with open('junktrades/trades_to_receive.json') as f:
+            trades = json.load(f)
+        for team, players, in trades.items():
+            print(f'Team {team}: {[id_to_name(player) for player in players]}')
 
     def trade_spam(self, targets, n, interval):
         """
@@ -227,3 +392,9 @@ class TraderBot:
 
     def shutdown(self):
         self.driver.quit()
+
+
+if __name__ == '__main__':
+    bot = TraderBot(93180, 3)
+    print(bot.team_id_to_name(4))
+    bot.shutdown()
